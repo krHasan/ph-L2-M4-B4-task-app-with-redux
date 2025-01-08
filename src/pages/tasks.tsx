dc4054f1298e6e69d4fdb017b6159fe6e9ecdad9
Rakib <hasanbappi@gmail.com>
2025-01-08 08:04:58 +0600

    feat(Tasks): create task store slice and initialize UI design

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,3 +1,8 @@
+import { selectTasks } from "@/redux/features/task/taskSlice";
+import { useAppSelector } from "@/redux/hook";
+
 export default function Tasks() {
+    const tasks = useAppSelector(selectTasks);
+    console.log(tasks);
     return <div>tasks</div>;
 }
---
+++ src/redux/features/task/taskSlice.ts
@@ -0,0 +1,32 @@
+import { RootState } from "@/redux/store";
+import { ITask } from "@/type";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+    tasks: ITask[];
+}
+
+const initialState: InitialState = {
+    tasks: [
+        {
+            id: "",
+            title: "Initial Task",
+            description: "Create Home page and routing",
+            dueDate: "2025-11",
+            isComplete: false,
+            priority: "High",
+        },
+    ],
+};
+
+const taskSlice = createSlice({
+    name: "task",
+    initialState,
+    reducers: {},
+});
+
+export const selectTasks = (state: RootState) => {
+    return state.todo.tasks;
+};
+
+export default taskSlice.reducer;
